f1f9c7012a4274f3c5c899bc15969cd92e6fcc1e
Ivan Sim <isim@redhat.com>
2021-06-09 11:09:57 -0700

    Add upgrade spec and status properties
    Signed-off-by: Ivan Sim <isim@redhat.com>

--- upgrade/kubelet/pkg/apis/isim.dev/v1alpha1/types.go
+++ upgrade/kubelet/pkg/apis/isim.dev/v1alpha1/types.go
@@ -18,10 +18,47 @@ type KubeletUpgradeConfig struct {
 }
 
 // KubeletUpgradeConfigSpec represents the spec of the upgrade process
-type KubeletUpgradeConfigSpec struct{}
+type KubeletUpgradeConfigSpec struct {
+	FailurePolicy  UpgradeFailurePolicy `json:"failurePolicy"`
+	Schedule       UpgradeSchedule      `json:"schedule"`
+	Selector       metav1.LabelSelector `json:"selector"`
+	Strategy       UpgradeStrategy      `json:"strategy"`
+	MaxUnavailable int                  `json:"maxUnavailable"`
+}
+
+// UpgradeSchedule defines the schedule in cron format.
+type UpgradeSchedule string
+
+// UpgradeStrategy defines the strategies to execute the upgrade i.e. node
+// replacement or inplace upgrade.
+type UpgradeStrategy int
+
+const (
+	UpgradeStrategyInplace UpgradeStrategy = iota
+	UpgradeStrategyReplace
+)
+
+// UpgradeFailurePolicy defines the policy to deal with upgrade failures i.e.
+// terminate process or process to next node.
+type UpgradeFailurePolicy int
+
+const (
+	UpgradeFailurePolicyHalt UpgradeFailurePolicy = iota
+	UpgradeFailurePolicyIgnore
+)
 
 // KubeletUpgradeConfigStatus represents the status of the upgrade process.
-type KubeletUpgradeConfigStatus struct{}
+type KubeletUpgradeConfigStatus struct {
+	History []UpgradeHistory `json:"history"`
+}
+
+// UpgradeHistory shows the history of a kubelet upgrade.
+type UpgradeHistory struct {
+	LastScheduledTime *metav1.Time `json:"lastScheduledTime"`
+	LastCompletedTime *metav1.Time `json:"lastCompletedTime"`
+	Node              string       `json:"node"`
+	Outcome           string       `json:"outcome"`
+}
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
